b7f2935e58e302aade6ae824cd41d3fcd6acfcb4
zknos777 <zknos777@gmail.com>
2024-06-13 20:33:44 +0300

    For PR

--- main.py
+++ main.py
@@ -20,49 +20,38 @@ from aiogram.types import (
 
 
 class Form(StatesGroup):
-    Header = State()
-    Text = State()
+    header = State()
+    text = State()
     link_text = State()
     link_url = State()
 
 dp = Dispatcher()
-
+form_router = Router(name=__name__)
 
 
 
 @form_router.message(CommandStart())
 async def command_start(message: Message, state: FSMContext) -> None:
-    await state.set_state(Form.name)
+    await state.set_state(Form.header)
     await message.answer(
-        "Hi there! What's your name?",
+        "Привет. Выбери шапку сообщений!",
         reply_markup=ReplyKeyboardRemove(),
     )
-@dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-    """
-    This handler receives messages with `/start` command
-    """
-    # Most event objects have aliases for API methods that can be called in events' context
-    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
-    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
-    # method automatically or call API method directly via
-    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
-    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+@form_router.message(Form.header)
+async def process_name(message: Message, state: FSMContext) -> None:
+    await state.update_data(name=message.text)
+    await state.set_state(Form.text)
+    await message.answer(
+        f"Отлично, {html.quote(message.text)}!\nКакой текст соообщения?",
+    )
 
-
-@dp.message()
-async def echo_handler(message: Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
+@form_router.message(Form.text, F.text.casefold() == "yes")
+async def process_like_write_bots(message: Message, state: FSMContext) -> None:
+    await state.set_state(Form.link_text)
+    await message.reply(
+        "Текст ссылки?",
+        reply_markup=ReplyKeyboardRemove(),
+    )
 
 
 async def main() -> None:
